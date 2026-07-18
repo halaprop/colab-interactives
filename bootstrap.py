a2c6bac2dbba10c1994f7b3aaefbdcf2c1244c0a
Daniel Halabe <dan@halabe.com>
2026-07-18 10:24:31 -0700

    even skinnier bootstrap

--- bootstrap.py
+++ bootstrap.py
@@ -1,13 +1,7 @@
-# Paste into your notebook's first cell, filling in your own repo below.
-# Fetches shim.py fresh and imports it as `shim` -- re-run this cell (not
-# just the show() cells) whenever shim.py itself changes.
-GITHUB_USER = 'halaprop'
-GITHUB_REPO = 'colab-interactives'
-REF = 'main'  # or a pinned tag
-
+# This code enables the interactive elements on this page. It can be disregarded. 
 import urllib.request, sys
 url = f'https://raw.githubusercontent.com/{GITHUB_USER}/{GITHUB_REPO}/{REF}/shim.py'
 urllib.request.urlretrieve(url, 'shim.py')
 sys.modules.pop('shim', None)
 import shim
-shim.GITHUB_USER, shim.GITHUB_REPO, shim.REF = GITHUB_USER, GITHUB_REPO, REF
+shim.GITHUB_USER, shim.GITHUB_REPO, shim.REF = 'halaprop', 'colab-interactives', 'main'
