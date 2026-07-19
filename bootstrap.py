df9372a2c96cc25df71198f6d057a4b3d8e4f858
Daniel Halabe <dan@halabe.com>
2026-07-18 20:57:43 -0700

    fixed and reduced bootstrapper

--- bootstrap.py
+++ bootstrap.py
@@ -1,7 +1,6 @@
-# This code enables the interactive elements on this page. It can be disregarded. 
+# This code enables the interactive elements on this page. It can be disregarded.
 import urllib.request, sys
-url = f'https://raw.githubusercontent.com/{GITHUB_USER}/{GITHUB_REPO}/{REF}/shim.py'
-urllib.request.urlretrieve(url, 'shim.py')
+urllib.request.urlretrieve('https://raw.githubusercontent.com/halaprop/colab-interactives/main/shim.py', 'shim.py')
 sys.modules.pop('shim', None)
 import shim
 shim.GITHUB_USER, shim.GITHUB_REPO, shim.REF = 'halaprop', 'colab-interactives', 'main'
